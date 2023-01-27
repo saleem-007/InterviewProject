7b5b5fee017442f865ecf01d57da2fe3a1024c59
Saleem M <sajb8722@gmail.com>
2023-01-27 12:45:00 +0530

    Added testNG.xml File and reformated the other classes code

--- src/main/java/Tests/BaseClass.java
+++ src/main/java/Tests/BaseClass.java
@@ -1,13 +1,9 @@
 package Tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
@@ -15,44 +11,44 @@ import java.util.concurrent.TimeUnit;
 public class BaseClass {
     WebDriver driver;
     WebDriverWait wait;
-    public void launchBrowser()
-    {
-      System.setProperty("webdriver.chrome.driver","G:\\chromedriver_win32\\chromedriver.exe");
-      driver=new ChromeDriver();
-      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-      driver.manage().window().maximize();
-      wait=new WebDriverWait(driver, 20);
+
+    public void launchBrowser() {
+        System.setProperty("webdriver.chrome.driver", "G:\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 20);
     }
-    public void waitAndClick(String xPath)
-    {
+
+    public void waitAndClick(String xPath) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPath))).click();
     }
-    public void waitAndSendKeys(String xPath, String sendingValue)
-    {
+
+    public void waitAndSendKeys(String xPath, String sendingValue) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPath))).sendKeys(sendingValue);
     }
-    public String waitAndGetTheText(String xPath)
-    {
-       String textOfTheElement= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPath))).getText();
-       return textOfTheElement;
+
+    public String waitAndGetTheText(String xPath) {
+        String textOfTheElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPath))).getText();
+        return textOfTheElement;
     }
-    public void clickOnDisabledElement(WebElement element)
-    {
+
+    public void clickOnDisabledElement(WebElement element) {
         JavascriptExecutor executor = (JavascriptExecutor) driver;
         executor.executeScript("arguments[0].click()", element);
     }
-    public void enterURL(String URL)
-    {
+
+    public void enterURL(String URL) {
         driver.get(URL);
     }
-    public WebElement element(String element)
-    {
-            WebElement ele= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(element)));
-            return ele;
+
+    public WebElement element(String element) {
+        WebElement ele = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(element)));
+        return ele;
     }
-    public void openNewTabAndEnterURL(String element)
-    {
-        ((JavascriptExecutor)driver).executeScript("window.open()");
+
+    public void openNewTabAndEnterURL(String element) {
+        ((JavascriptExecutor) driver).executeScript("window.open()");
         ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
         enterURL(element);
--- src/main/java/Tests/PushpaReleaseDateTest.java
+++ src/main/java/Tests/PushpaReleaseDateTest.java
@@ -9,16 +9,17 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class PushpaReleaseDateTest extends BaseClass{
+public class PushpaReleaseDateTest extends BaseClass {
     ImdbPage imdb;
     WikiPage wikiPage;
+
     @BeforeClass
-    public void setUp()
-    {
+    public void setUp() {
         launchBrowser();
-        imdb=new ImdbPage();
-        wikiPage=new WikiPage();
+        imdb = new ImdbPage();
+        wikiPage = new WikiPage();
     }
+
     @Test
     public void verifyThePushpaReleaseDateAndCountry() throws InterruptedException {
         enterURL(imdb.imdbPageURL);
@@ -28,40 +29,38 @@ public class PushpaReleaseDateTest extends BaseClass{
         if (name.contains(imdb.pushpaTheRiseText)) {
             waitAndClick(imdb.push1FirstResult);
         }
-        WebElement releaseDateElement =wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(imdb.pushpaMovieReleaseDate)));
+        WebElement releaseDateElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(imdb.pushpaMovieReleaseDate)));
         String releaseDateElementText = releaseDateElement.getText();
-        String pushpaReleaseDateInImdb="";
-        if(!releaseDateElementText.contains("India")) {
+        String pushpaReleaseDateInImdb = "";
+        if (!releaseDateElementText.contains("India")) {
             clickOnDisabledElement(releaseDateElement);
         }
         try {
             if (element(imdb.imdbIndiaReleaseDate).isDisplayed()) {
                 pushpaReleaseDateInImdb = waitAndGetTheText(imdb.imdbIndiaReleaseDate);
             }
-        }catch(Exception e)
-        {
+        } catch (Exception e) {
             clickOnDisabledElement(element(imdb.seeMoreItemsButton));
             pushpaReleaseDateInImdb = waitAndGetTheText(imdb.indiaReleaseDate);
         }
         openNewTabAndEnterURL(wikiPage.wikiURL);
-        waitAndSendKeys(wikiPage.searchTextField,imdb.pushpaMovieName);
+        waitAndSendKeys(wikiPage.searchTextField, imdb.pushpaMovieName);
         waitAndClick(wikiPage.suggestedListFirstElement);
-        String releaseInDateInWiki=waitAndGetTheText(wikiPage.pushpaReleaseDateInWiki);
-        String releaseCountryDataInWiki=waitAndGetTheText(wikiPage.pushpaReleaseCountry);
-        pushpaReleaseDateInImdb=pushpaReleaseDateInImdb.replace(",","").replace("\n", " ");
-        releaseCountryDataInWiki=releaseInDateInWiki+" "+releaseCountryDataInWiki;
-        releaseCountryDataInWiki=releaseCountryDataInWiki.replace("\n"," ");
-        String [] str=pushpaReleaseDateInImdb.split(" ");
-        for (String parts:str)
-        {
-            if(!releaseCountryDataInWiki.contains(parts)) {
+        String releaseInDateInWiki = waitAndGetTheText(wikiPage.pushpaReleaseDateInWiki);
+        String releaseCountryDataInWiki = waitAndGetTheText(wikiPage.pushpaReleaseCountry);
+        pushpaReleaseDateInImdb = pushpaReleaseDateInImdb.replace(",", "").replace("\n", " ");
+        releaseCountryDataInWiki = releaseInDateInWiki + " " + releaseCountryDataInWiki;
+        releaseCountryDataInWiki = releaseCountryDataInWiki.replace("\n", " ");
+        String[] str = pushpaReleaseDateInImdb.split(" ");
+        for (String parts : str) {
+            if (!releaseCountryDataInWiki.contains(parts)) {
                 Assert.fail();
             }
-            }
         }
+    }
+
     @AfterClass
-        public void tearDown()
-        {
-            driver.quit();
+    public void tearDown() {
+        driver.quit();
     }
 }
